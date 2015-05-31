9cb222fdaac510757266b2f1c2fa3b0358850a1e
Weslley Araujo <weslleyaraujo20@gmail.com>
2015-05-31 06:35:35 -0500

    feature: moving styles to single file, creating flip effect

--- field.js
+++ field.js
@@ -1,41 +1,69 @@
 ;(function (root) {
 
-	function Field (options) {
-		this.options = options;
-		this.prepare();
-		this.bind();
-		this.start();
-		return this;
-	}
+  function Field (options) {
+    this.options = options;
+    this.prepare();
+    this.bind();
+    this.start();
+    return this;
+  }
 
-	Field.prototype.bind = function () {
-		this.el.addEventListener('click', this.onClick.bind(this), false);
-	};
+  Field.prototype.bind = function () {
+    this.el.addEventListener('click', this.onClick.bind(this), false);
+  };
 
-	Field.prototype.onClick = function (event) {
-		this.toggleClass(event.currentTarget, 'is-active');
-		return false;
-	};
+  Field.prototype.onClick = function (event) {
+    this.toggleClass(event.currentTarget, 'is-active');
 
-	Field.prototype.toggleClass = function (el, className) {
-		var regex = new RegExp(className, 'g');
-		if (el.className.match(regex)) {
-			el.className = el.className.replace(regex, '');
-			return;
-		}
-		
-		el.className  = (' ' + className);
-	};
+    /* already active? */
+    if (typeof this.afterClick === 'function') {
+      this.afterClick();
+    }
 
-	Field.prototype.prepare = function () {
-		this.el = document.createElement('td');
-		this.name = this.options.name;
-	};
+    return false;
+  };
 
-	Field.prototype.start = function () {
-		this.el.innerHTML = this.name;
-	};
-	
-	root.Field = Field;
+  Field.prototype.registerClick = function (fn) {
+    this.afterClick = fn;
+  };
+
+  Field.prototype.hasClass = function(el, className) {
+    var regex = new RegExp(className, 'g');
+    return el.className.match(regex);
+  };
+
+  Field.prototype.toggleClass = function (el, className) {
+    var regex = new RegExp(className, 'g');
+    if (el.className.match(regex)) {
+      el.className = el.className.replace(regex, '');
+      return;
+    }
+
+    el.className  = (' ' + className);
+  };
+
+  Field.prototype.createElements = function () {
+    this.front = document.createElement('figure');
+    this.back  = document.createElement('figure');
+    this.el    = document.createElement('td');
+
+    this.front.className = 'is-front';
+    this.back.className = 'is-back';
+
+    this.el.appendChild(this.front);
+    this.el.appendChild(this.back);
+  };
+
+  Field.prototype.prepare = function () {
+    this.createElements();
+    this.name = this.options.name;
+  };
+
+  Field.prototype.start = function () {
+    this.back.innerHTML = this.name;
+    this.front.innerHTML = "lol";
+  };
+
+  root.Field = Field;
 
 } (window));
