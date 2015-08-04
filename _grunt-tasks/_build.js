414f217cbdfd40a6160dacf84be9e7c7fc3b1d18
Weslley Araujo <weslleyaraujo20@gmail.com>
2015-08-04 00:24:32 -0300

    [FEATURE] Build proccess just fine! 🎉

--- _grunt-tasks/_build.js
+++ _grunt-tasks/_build.js
@@ -10,6 +10,7 @@ module.exports = function(grunt) {
     'uglify',
     'render',
     'htmlmin',
+    'replace',
   ]);
 
 };
---
+++ _grunt-tasks/replace.js
@@ -0,0 +1,37 @@
+/*
+ * FIXME: Think in some smarter way to do this
+ */
+module.exports = function(grunt) {
+  'use strict';
+
+  var CONFIG_NAME = 'config' + (grunt.option('config') ? '-' + grunt.option('config') : '-development') + '.json';
+  var CONFIG_FILE = grunt.file.readJSON(CONFIG_NAME);
+
+  grunt.config('replace', {
+    dist: {
+      options: {
+        patterns: [
+          {
+            match: /\/src\/fonts\//g,
+            replacement: CONFIG_FILE.fonts.src
+          }
+        ]
+      },
+      files: [
+        {
+          expand: true,
+          flatten: true,
+          src: ['dist/css/*.css'],
+          dest: 'dist/css/'
+        }
+      ]
+    }
+  });
+
+  grunt.registerTask('replace', function() {
+    grunt.task.run(['replace']);
+  });
+
+  grunt.loadNpmTasks('grunt-replace');
+
+};
