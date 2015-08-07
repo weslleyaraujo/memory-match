e06406feb9d854b9c4b7387914324918d2bc6872
Weslley Araujo <weslleyaraujo20@gmail.com>
2015-08-07 11:48:50 -0300

    [FIX] Font icon #24, some levels details

--- src/js/config/levels.js
+++ src/js/config/levels.js
@@ -1,27 +1,21 @@
 define([], function () {
   return [
     {
-      index: 0,
-      name: 'dumb',
-      size: 2,
-    },
-
-    {
       index: 1,
       name: 'easy',
-      size: 4,
+      size: 2,
     },
 
     {
       index: 2,
       name: 'medium',
-      size: 6,
+      size: 4,
     },
 
     {
       index: 3,
       name: 'hard',
-      size: 4,
+      size: 6,
     },
 
     {
